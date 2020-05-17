3145490def283f79a99632ca9ac378c851b04357
Rylan Zhou <oakenshield@RylanZ-MacBook-Pro.local>
2020-05-17 22:29:26 +0800

    Image Comparison Page following @DarkCode

--- Responsive Navbar Page/app.js
+++ Responsive Navbar Page/app.js
@@ -1,25 +1,26 @@
 /* eslint-disable no-param-reassign */
 const navSlide = () => {
-  const burger = document.querySelector('.burger');
-  const navLinksGroup = document.querySelector('.nav-link');
-  const navLinks = document.querySelectorAll('.nav-link li');
+  const burger = document.querySelector('.burger')
+  const navLinksGroup = document.querySelector('.nav-link')
+  const navLinks = document.querySelectorAll('.nav-link li')
 
   burger.addEventListener('click', () => {
     // Links panel animation
-    navLinksGroup.classList.toggle('nav-active');
+    navLinksGroup.classList.toggle('nav-active')
 
     // Links animation
     navLinks.forEach((link, index) => {
       if (!link.style.animation) {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +
+          0.5}s`
       } else {
-        link.style.animation = '';
+        link.style.animation = ''
       }
-    });
+    })
 
     // Burger
-    burger.classList.toggle('toggle');
-  });
-};
+    burger.classList.toggle('toggle')
+  })
+}
 
-navSlide();
+navSlide()
