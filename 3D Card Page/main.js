208fffb6cd250edaadb6b4d9db24a19a56fa85b5
YunanZhou <yunan.zhou@shopee.com>
2020-11-04 17:32:19 +0800

    Fix js and styling

--- 3D Card Page/main.js
+++ 3D Card Page/main.js
@@ -2,13 +2,15 @@ const container = document.querySelector('.container')
 const card = document.querySelector('.card')
 const title = document.querySelector('.title')
 const sneaker = document.querySelector('.sneaker img')
-const purchase = document.querySelector('.purchase button')
+const purchase = document.querySelector('.purchase')
 const description = document.querySelector('.info h3')
 const sizes = document.querySelector('.sizes')
 
+const RATIO = 25
+
 container.addEventListener('mousemove', (e) => {
-  let xAxis = (window.innerWidth / 2 - e.pageX) / 25
-  let yAxis = (window.innerHeight / 2 - e.pageY) / 25
+  let xAxis = (window.innerWidth / 2 - e.pageX) / RATIO
+  let yAxis = (window.innerHeight / 2 - e.pageY) / RATIO
 
   card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
 })
@@ -22,7 +24,7 @@ container.addEventListener('mouseenter', (e) => {
   sneaker.style.transform = 'translateZ(200px) rotateZ(-45deg)'
   description.style.transform = 'translateZ(125px)'
   sizes.style.transform = 'translateZ(100px)'
-  purchase.style.transform = 'translateZ(75px)'
+  purchase.style.transform = 'translateZ(100px)'
 })
 
 // Animate Out
